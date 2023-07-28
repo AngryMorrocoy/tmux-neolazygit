6e9f0d9d5a1e98fdfdc92626ea12ac54a56002ed
Manuel Rivas <mrivasmahecha@gmail.com>
2023-07-28 08:44:11 -0400

    (+) Base of the editor wrapper for sending files to the nvim instance in the tmux pane from where the command was called

---
+++ scripts/editor.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# A script that opens nvim from lazygit, if it was opened from a tmux pane, checks
+# if exists a nvim remote server for that pane
+
+# "editor.sh {{editor}} {{filename}} {{line}}"
+
+FILENAME=$1
+LINE=${2:-0}
+
+# echo "Origin: $LAZYGIT_ORIGIN_PANE"
+
+# Check if there's a nvim instance in the origin pane and 'returns' its server socket,
+# otherwise returns 0
+get_nvim_socket () {
+    local pid_of_origin=$(tmux list-panes -sF "#{pane_pid}" \
+                            -f "#{m:#{pane_id},${LAZYGIT_ORIGIN_PANE}}")
+
+    if [ -z $pid_of_origin ]; then
+        echo 0
+        return
+    fi
+
+    # Gets nvim pid on the pane of origin
+    nvim_pid=$(pgrep -P "$pid_of_origin" vim)
+
+    if [ -z $nvim_pid ]; then
+        echo 0
+        return
+    fi
+
+    nvim_socket=$(ls ${XDG_RUNTIME_DIR}/nvim* 2>/dev/null | grep $nvim_pid)
+
+    if [ -z $nvim_socket ]; then
+        echo 0
+        return
+    fi
+
+    echo $nvim_socket
+}
+
+
+main() {
+    socket=$(get_nvim_socket)
+    # If no socket, it means no nvim, so just open inside lazygit ;)
+    if [[ $socket == 0 ]]; then
+        nvim +$LINE "$FILENAME"
+        exit 0
+    fi
+}
+
+main
--- scripts/open-lazygit.sh
+++ scripts/open-lazygit.sh
@@ -1,10 +1,17 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CUSTOM_LAZYGIT_CONFIG="$CURRENT_DIR/../lazygit/config.yml"
 
+LAZYGIT_EDITOR="$CURRENT_DIR/editor.sh" # Check usage in lazygit/config.yml
+LAZYGIT_CONFIG=$(echo "$(lazygit -cd)/config.yml")
+
 openLazygit () {
-    local LAZYGIT_CONFIG=$(echo "$(lazygit -cd)/config.yml")
+    # Gets the pane id from where the script was called
+    local LAZYGIT_ORIGIN_PANE=($(tmux display-message -p "#D"))
+
     # Opens a new tmux window running lazygit appending the needed config
     tmux neww \
+        -e LAZYGIT_EDITOR=$LAZYGIT_EDITOR \
+        -e LAZYGIT_ORIGIN_PANE=$LAZYGIT_ORIGIN_PANE \
         lazygit \
         -ucf $LAZYGIT_CONFIG,$CUSTOM_LAZYGIT_CONFIG
 }
